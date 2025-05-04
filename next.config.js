c28d95d0e837c8a7eed86bbb3bdc6fabad5e440a
Whitefox980 <ivannestorov80@gmail.com>
2025-05-05 00:06:05 +0200

    Vracen ask.ts API endpoint

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
